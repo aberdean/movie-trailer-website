58a8b1e0b680e9c95ea8e3d0c22a41938c6aff9c
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2018-03-03 00:42:06 +0100

    Fix layout on large screens

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -35,6 +35,7 @@ main_page_head = '''
         .movie-tile {
             margin-bottom: 20px;
             padding-top: 20px;
+            min-height: 54%;
         }
         .movie-tile:hover {
             background-color: #EEE;
